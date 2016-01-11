da573ec38a3e50efbdf3c475417634d89d6f1fc4
Techjar <tecknojar@gmail.com>
2016-01-10 22:08:24 -0500

    Added more gradient animations

--- src/com/techjar/ledcm/hardware/animation/AnimationGradient.java
+++ src/com/techjar/ledcm/hardware/animation/AnimationGradient.java
@@ -5,7 +5,10 @@ import com.techjar.ledcm.LEDCubeManager;
 import com.techjar.ledcm.util.MathHelper;
 import com.techjar.ledcm.util.Timer;
 import com.techjar.ledcm.util.Util;
+import com.techjar.ledcm.util.Vector2;
+import com.techjar.ledcm.util.Vector3;
 import org.lwjgl.util.Color;
+import org.lwjgl.util.vector.Vector3f;
 
 /**
  *
@@ -17,7 +20,7 @@ public class AnimationGradient extends Animation {
     private Color color2 = new Color(0, 0, 0);
     private float speed = 10;
     private float scale = 3;
-    private boolean pulsate;
+    private int animMode = 0;
     private int colorMode = 0;
 
     public AnimationGradient() {
@@ -31,21 +34,43 @@ public class AnimationGradient extends Animation {
 
     @Override
     public synchronized void refresh() {
-        for (int y = 0; y < dimension.y; y++) {
-            float value = y / (dimension.y - 1F);
-            Color color = new Color();
-            if (colorMode == 0) {
-                if (pulsate) value = (float)Math.sin(timer.getSeconds() * (speed / 5) + (((dimension.y - 1) - y) / (dimension.y - 1F)) * scale) * 0.5F + 0.5F;
-                color = MathHelper.lerpLab(color1, color2, value);
+        if (animMode == 3 || animMode == 4) {
+            Vector3 center = Util.convertVector(LEDCubeManager.getLEDCube().getCenterPoint());
+            for (int z = 0; z < dimension.z; z++) {
+                for (int y = 0; y < dimension.y; y++) {
+                    for (int x = 0; x < dimension.x; x++) {
+                        Color color = new Color();
+                        float dist = new Vector3(x, y, z).distance(center);
+                        if (colorMode == 0) {
+                            float value = (float)Math.sin(timer.getSeconds() * (speed / 5) + (animMode == 3 ? dist : -dist) * scale / 2) * 0.5F + 0.5F;
+                            color = MathHelper.lerpLab(color1, color2, value);
+                        } else if (colorMode == 1) {
+                            float value = ((float)timer.getSeconds() * (speed * 0.0142857F) + (animMode == 3 ? dist : -dist) * (scale * 0.0666667F) / 2) % 1;
+                            color.fromHSB(value, 1, 1);
+                        }
+                        ledManager.setLEDColor(x, y, z, color);
+                    }
+                }
             }
-            else if (colorMode == 1) {
-                if (pulsate) value = (((float)timer.getSeconds() * (speed * 0.0142857F) + (((dimension.y - 1) - y) / (dimension.y - 1F)) * (scale * 0.0666667F))) % 1;
-                else value *= (300F / 360F);
-                color.fromHSB(value, 1, 1);
-            }
-            for (int x = 0; x < dimension.x; x++) {
-                for (int z = 0; z < dimension.z; z++) {
-                    ledManager.setLEDColor(x, y, z, color);
+        } else {
+            for (int y = 0; y < dimension.y; y++) {
+                float value = y / (dimension.y - 1F);
+                Color color = new Color();
+                if (colorMode == 0) {
+                    if (animMode == 1) value = (float)Math.sin(timer.getSeconds() * (speed / 5) + (((dimension.y - 1) - y) / (dimension.y - 1F)) * scale) * 0.5F + 0.5F;
+                    else if (animMode == 2) value = (float)Math.sin(timer.getSeconds() * (speed / 5)) * 0.5F + 0.5F;
+                    color = MathHelper.lerpLab(color1, color2, value);
+                }
+                else if (colorMode == 1) {
+                    if (animMode == 1) value = ((float)timer.getSeconds() * (speed * 0.0142857F) + (((dimension.y - 1) - y) / (dimension.y - 1F)) * (scale * 0.0666667F)) % 1;
+                    else if (animMode == 2) value = ((float)timer.getSeconds() * (speed * 0.0142857F)) % 1;
+                    else value *= (300F / 360F);
+                    color.fromHSB(value, 1, 1);
+                }
+                for (int x = 0; x < dimension.x; x++) {
+                    for (int z = 0; z < dimension.z; z++) {
+                        ledManager.setLEDColor(x, y, z, color);
+                    }
                 }
             }
         }
@@ -61,8 +86,8 @@ public class AnimationGradient extends Animation {
         return new AnimationOption[]{
             new AnimationOption("color1", "Start Color", AnimationOption.OptionType.COLORPICKER, new Object[]{color1}),
             new AnimationOption("color2", "End Color", AnimationOption.OptionType.COLORPICKER, new Object[]{color2}),
+            new AnimationOption("animmode", "Animation", AnimationOption.OptionType.COMBOBOX, new Object[]{animMode, 0, "Static", 1, "Scroll", 2, "Fade", 3, "Radiate In", 4, "Radiate Out"}),
             new AnimationOption("colormode", "Color Mode", AnimationOption.OptionType.COMBOBOX, new Object[]{colorMode, 0, "Bicolor", 1, "Rainbow"}),
-            new AnimationOption("pulsate", "Pulsate", AnimationOption.OptionType.CHECKBOX, new Object[]{pulsate}),
             new AnimationOption("speed", "Speed", AnimationOption.OptionType.SLIDER, new Object[]{(speed - 1) / 149F}),
             new AnimationOption("scale", "Scale", AnimationOption.OptionType.SLIDER, new Object[]{(29 - (scale - 1)) / 29F}),
         };
@@ -77,11 +102,11 @@ public class AnimationGradient extends Animation {
             case "color2":
                 color2 = Util.stringToColor(value);
                 break;
+            case "animmode":
+                animMode = Integer.parseInt(value);
+                break;
             case "colormode":
                 colorMode = Integer.parseInt(value);
-                break;
-            case "pulsate":
-                pulsate = Boolean.parseBoolean(value);
                 break;
             case "speed":
                 speed = 1 + (149 * Float.parseFloat(value));
